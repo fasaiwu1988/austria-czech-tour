621f0e42edbe7265e81ea144cbf230f3502ff2a4
fasaiwu1988 <fasaiwu1988@gmail.com>
2026-06-02 16:07:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const reveals = document.querySelectorAll(".reveal");
 
+window.addEventListener("scroll", () => {
+
+reveals.forEach(item => {
+
+const top = item.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+item.classList.add("active");
+}
+
+});
+
+});
+
+const accordionBtns =
+document.querySelectorAll(".accordion-btn");
+
+accordionBtns.forEach(btn=>{
+
+btn.addEventListener("click",()=>{
+
+const content = btn.nextElementSibling;
+
+if(content.style.display==="block"){
+content.style.display="none";
+}else{
+content.style.display="block";
+}
+
+});
+
+});
